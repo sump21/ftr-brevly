5848ba0f339e9c434c4e239a3b2299f23715ea8d
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-28 17:08:56 -0300

    add env

---
+++ server/src/env.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+	PORT: z.coerce.number().default(3000),
+	NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+	DATABASE_URL: z.string().url().startsWith("postgresql://"),
+	DATABASE_PORT: z.coerce.number().default(5433),
+
+	CLOUDFLARE_ACCOUNT_ID: z.string(),
+	CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+	CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+	CLOUDFLARE_BUCKET: z.string(),
+	CLOUDFLARE_PUBLIC_URL: z.string().url(),
+});
+
+export const env = envSchema.parse(process.env);
