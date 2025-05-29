e20d6b81bdca14402843784f4575dde6c081ea8e
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-29 07:55:20 -0300

    add font and images

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,14 @@
+import { theme } from "tailwindcss/defaultConfig";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Open Sans", ...theme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
