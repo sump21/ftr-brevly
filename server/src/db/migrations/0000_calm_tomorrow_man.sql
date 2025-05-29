4eae9d3f71d7a788534259d338cc78d437f5e0cd
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-29 00:38:22 -0300

    fix migrate

--- server/src/db/migrations/0000_calm_tomorrow_man.sql
+++ server/src/db/migrations/0000_calm_tomorrow_man.sql
@@ -3,6 +3,6 @@ CREATE TABLE "links" (
 	"original_link" text NOT NULL,
 	"short_link" text NOT NULL,
 	"access_count" integer DEFAULT 0 NOT NULL,
-	"created_at" timestamp DEFAULT now(),
+	"created_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "links_short_link_unique" UNIQUE("short_link")
 );
