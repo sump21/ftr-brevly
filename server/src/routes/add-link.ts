919c3a92e4fbf266ea4146d1e4d3bc106937dcd5
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-28 23:06:05 -0300

    function add_link added

--- server/src/routes/add-link.ts
+++ server/src/routes/add-link.ts
@@ -1,9 +1,25 @@
+import { addLink } from "@/functions/add-link";
 import type { FastifyInstance } from "fastify";
 
 export async function addLinkRoute(app: FastifyInstance) {
 	app.post("/add-link", async (request, reply) => {
-		return {
-			message: "Link added successfully",
+		const { originalLink, shortLink } = request.body as {
+			originalLink: string;
+			shortLink: string;
 		};
+
+		if (!originalLink || !shortLink) {
+			return reply
+				.status(400)
+				.send({ error: "originalLink AND shortLink are required" });
+		}
+
+		const result = await addLink({ originalLink, shortLink });
+
+		if (!result) {
+			return reply.status(500).send({ error: "Failed to add link" });
+		}
+
+		return reply.status(201).send();
 	});
 }
