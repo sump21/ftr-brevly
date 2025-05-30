78628a92057571fd26aaf9ce8f4c99596e9d1c36
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-30 01:13:32 -0300

    add svg not-found

--- web/src/pages/not-found.tsx
+++ web/src/pages/not-found.tsx
@@ -1,12 +1,10 @@
+import { NotFound } from "../shared/not-found";
+
 export function NotFoundPage() {
 	return (
 		<div className="flex flex-col h-dvh w-full p-2 sm:max-w-[580px] mx-auto items-center justify-center sm:w-max">
 			<div className="w-full flex flex-col items-center justify-center py-12 px-5 sm:py-16 sm:px-12 bg-white rounded-lg">
-				<img
-					src="images/404.png"
-					alt="not-found"
-					className="h-16 sm:h-20 mb-4"
-				/>
+				<NotFound />
 				<span className="text-gray-600 text-xl font-bold mb-6 text-center">
 					Link não encontrado
 				</span>
