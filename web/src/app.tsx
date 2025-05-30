192fac41893f468a033ae3487ca3fbcf62da6949
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-30 00:32:24 -0300

    add routes

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,5 +1,20 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { queryClient } from "./api";
 import { HomePage } from "./pages/home";
+import { NotFoundPage } from "./pages/not-found";
+import { RedirectPage } from "./pages/redirect";
 
 export function App() {
-	return <HomePage />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<HomePage />} />
+					<Route path="/:shortUrl" element={<RedirectPage />} />
+					<Route path="/not-found" element={<NotFoundPage />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
+	);
 }
