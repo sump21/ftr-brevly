7bcfb234933e0f3a0ebc4fe6b9ae76697b7a64bc
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-30 14:54:55 -0300

    update form

--- web/src/components/link-form.tsx
+++ web/src/components/link-form.tsx
@@ -1,51 +1,94 @@
-import { WarningIcon } from "@phosphor-icons/react";
+import { z } from "zod";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { addLinks } from "../api/add-link";
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useForm} from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from 'sonner'
+
+const addLinkInput = z.object({
+  originalLink: z.string().url("Informe uma URL válida."),
+  shortLink: z.string().regex(
+			/^[a-zA-Z0-9_-]+$/,
+			"Informe uma url minúscula e sem espaço/caracter especial.",
+		),
+});
+
+type AddLinkInput = z.infer<typeof addLinkInput>;
 
 export function LinkForm() {
+
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors, isSubmitting },
+	} = useForm<AddLinkInput>({
+		resolver: zodResolver(addLinkInput),
+		defaultValues: {
+			originalLink: "",
+			shortLink: "",
+		}
+	});
+
+	const queryClient = useQueryClient()
+
+	const { mutate: addLinkMutation } = useMutation({
+    mutationFn: addLinks,
+    onMutate: () => {
+        toast.loading("Adicionando link...")
+    },
+    onSuccess: () => {
+        toast.success("Link adicionado com sucesso!")
+        reset()
+        queryClient.invalidateQueries({ queryKey: ['links'] })
+    },
+    onError: (error) => {
+        console.log(error)
+        toast.error("Erro ao adicionar link")
+    }
+})
+
+async function handleSubmitForm(data: AddLinkInput) {
+    console.log(data)
+    addLinkMutation({
+        originalLink: data.originalLink,
+        shortLink: data.shortLink,
+    })
+}
+
+	
 	return (
-		<div className="w-full sm:min-w-80 p-6 sm:p-8 flex flex-col justify-center bg-white rounded-lg">
-			<h2 className="flex text-xl text-grayscale-600 mb-3">Novo link</h2>
-			<form className="flex flex-col">
-				<div>
-					<span className="flex text-xs uppercase text-grayscale-500 mb-2">
-						Link Original
-					</span>
-					<input
+			<form onSubmit={handleSubmit(handleSubmitForm)} className="flex flex-col">
+
+				<Label text="Link original" />
+				<Input
 						type="text"
 						placeholder="www.exemplo.com.br"
-						className={
-							"w-full h-12 border border-grayscale-300 rounded-lg p-4 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-base placeholder:text-grayscale-400"
-						}
-					/>
-					<span className="flex items-start gap-2">
-						<WarningIcon size={16} className="fill-danger" />
-						<p className="text-sm text-grayscale-500">Error message</p>
-					</span>
-				</div>
+						onChange={(value) => (value)}
+						error={errors.originalLink?.message || ''}
+						disabled={false}
+						{...register("originalLink")}
+				/>
 
-				<div>
-					<span className="text-xs uppercase text-grayscale-500">
-						Link Encurtado
-					</span>
-					<div className="flex items-center border border-grayscale-300 rounded-md px-4 mb-2 focus-within:ring-2 focus-within:ring-blue-base">
-						<span className="text-grayscale-400">brev.ly/</span>
-						<input
-							type="text"
-							className="w-full h-12 outline-none border-none bg-transparent"
-						/>
-					</div>
-					<span className="flex items-start gap-2">
-						<WarningIcon size={16} className="fill-danger" />
-						<p className="text-sm text-grayscale-500">Error message</p>
-					</span>
-				</div>
+
+				<Label text="Link encurtado" />
+<Input
+    prefix="brev.ly/"
+    onChange={(value) => (value)}
+    error={errors.shortLink?.message || ''}
+    disabled={false}
+    {...register("shortLink")}
+/>
 
 				<button
 					type="submit"
 					className="flex flex-col justify-center w-full h-12 p-5 mt-2 bg-blue-base text-white rounded-lg hover:bg-blue-dark transition"
+					disabled={isSubmitting}
 				>
-					Salvar link
+					{isSubmitting ? "Salvando..." : "Salvar link"}
 				</button>
 			</form>
-		</div>
 	);
 }
