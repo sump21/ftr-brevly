10028f0a816e8f38c7c429b0b6a664a5b07fe92e
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-29 21:50:02 -0300

    add tailwind configs

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,6 +8,53 @@ export default {
 			fontFamily: {
 				sans: ["Open Sans", ...theme.fontFamily.sans],
 			},
+			fontSize: {
+				xs: [
+					"10px",
+					{
+						lineHeight: "14px",
+					},
+				],
+				sm: [
+					"12px",
+					{
+						lineHeight: "16px",
+						fontWeight: "500",
+					},
+				],
+				md: [
+					"14px",
+					{
+						lineHeight: "18px",
+						fontWeight: "600",
+					},
+				],
+				lg: [
+					"18px",
+					{
+						lineHeight: "24px",
+						fontWeight: "700",
+					},
+				],
+				xl: [
+					"24px",
+					{
+						lineHeight: "32px",
+						fontWeight: "700",
+					},
+				],
+			},
+			colors: {
+				"blue-base": "#2C46B1",
+				"blue-dark": "#2C4091",
+				"grayscale-100": "#F9F9FB",
+				"grayscale-200": "#E4E6EC",
+				"grayscale-300": "#CDCFD5",
+				"grayscale-400": "#74798B",
+				"grayscale-500": "#4D505C",
+				"grayscale-600": "#1F2025",
+				danger: "#B12C4D",
+			},
 		},
 	},
 	plugins: [],
