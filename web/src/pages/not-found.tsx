07ce8a46c96faf50c559d1f2a18d129cde69cbbc
Raphael Nascimento <raphael.rsn.123@gmail.com>
2025-05-30 00:12:40 -0300

    add not-found page

---
+++ web/src/pages/not-found.tsx
@@ -0,0 +1,24 @@
+export function NotFoundPage() {
+	return (
+		<div className="flex flex-col h-dvh w-full p-2 sm:max-w-[580px] mx-auto items-center justify-center sm:w-max">
+			<div className="w-full flex flex-col items-center justify-center py-12 px-5 sm:py-16 sm:px-12 bg-white rounded-lg">
+				<img
+					src="images/404.png"
+					alt="not-found"
+					className="h-16 sm:h-20 mb-4"
+				/>
+				<span className="text-gray-600 text-xl font-bold mb-6 text-center">
+					Link não encontrado
+				</span>
+				<span className="w-full text-md text-grayscale-500 text-center">
+					O link que você está tentando acessar não existe, foi removido ou é
+					uma URL inválida. Saiba mais em{" "}
+					<a href={"/"} className="text-blue-dark underline">
+						brev.ly
+					</a>
+					.
+				</span>
+			</div>
+		</div>
+	);
+}
